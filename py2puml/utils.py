e2270c679fe6aa1eaf2348daa43460052be4da2a
Luc Sorel-Giffo <lucsorel@users.noreply.github.com>
2020-06-10 14:44:26 +0200

    :bookmark: 0.3.0 handle classes derived from namedtuples (#6)

--- py2puml/utils.py
+++ py2puml/utils.py
@@ -1,18 +1,24 @@
 from typing import Type
 
-def inspect_type(type: Type):
+def inspect_type(type_to_inspect: Type):
     '''
     Utilitary function which inspects the annotations of the given type
     '''
-    type_annotations = getattr(type, '__annotations__', None)
+    type_annotations = getattr(type_to_inspect, '__annotations__', None)
     if type_annotations is None:
-        print(f'class {type.__module__}.{type.__name__} has no annotation')
+        # print(f'class {type_to_inspect.__module__}.{type_to_inspect.__name__} of type {type(type_to_inspect)} has no annotation')
+        for attr_class_key in dir(type_to_inspect):
+            if attr_class_key != '__doc__':
+                print(
+                    f'{type_to_inspect.__name__}.{attr_class_key}:',
+                    getattr(type_to_inspect, attr_class_key)
+                )
     else:
-        # print(type.__annotations__)
+        # print(type_to_inspect.__annotations__)
         for attr_name, attr_class in type_annotations.items():
             for attr_class_key in dir(attr_class):
                 if attr_class_key != '__doc__':
                     print(
-                        f'{type.__name__}.{attr_name}:',
+                        f'{type_to_inspect.__name__}.{attr_name}:',
                         attr_class_key, getattr(attr_class, attr_class_key)
                     )
---
+++ tests/modules/withnamedtuple.py
@@ -0,0 +1,3 @@
+from collections import namedtuple
+
+Circle = namedtuple('Circle', ['x', 'y', 'radius'], defaults=[1])
