f8f5b0a57d6c11131d8cb524f3d61662e4f2680a
doyou89.jung <doyou89.jung@samsung.com>
2021-01-11 17:29:19 +0900

    modify import for easy mocking

--- py2puml/py2puml.py
+++ py2puml/py2puml.py
@@ -1,7 +1,7 @@
 from typing import Dict, List, Iterable
 from types import ModuleType
-from pkgutil import walk_packages
-from importlib import import_module
+import pkgutil
+import importlib
 
 from py2puml.domain.umlitem import UmlItem
 from py2puml.domain.umlrelation import UmlRelation
@@ -12,11 +12,9 @@ from py2puml.exportpuml import to_puml_content
 def py2puml(domain_path: str, domain_module: str) -> Iterable[str]:
     domain_items_by_fqdn: Dict[str, UmlItem] = {}
     domain_relations: List[UmlRelation] = []
-    for _, name, is_pkg in walk_packages([domain_path], domain_module + '.'):
+    for _, name, is_pkg in pkgutil.walk_packages([domain_path], domain_module + '.'):
         if not is_pkg:
-            domain_item_module: ModuleType = import_module(
-                name
-            )
+            domain_item_module: ModuleType = importlib.import_module(name)
             parse_item_module(
                 domain_item_module,
                 domain_module,
