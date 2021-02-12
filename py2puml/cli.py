d5ca8442a06a122e236e81d4b9e506b862be01db
Julien Jerphanion <git@jjerphan.xyz>
2021-02-12 20:47:35 +0100

    Respect PEP8 using single quote

--- py2puml/cli.py
+++ py2puml/cli.py
@@ -8,18 +8,18 @@ from py2puml import __version__
 
 
 def run():
-    argparser = argparse.ArgumentParser(description="Generate PlantUML diagrams to document your python code.")
+    argparser = argparse.ArgumentParser(description='Generate PlantUML diagrams to document your python code.')
 
     argparser.add_argument('-v', '--version', action='version',
                            version='py2puml {version}'.format(version=__version__))
 
-    argparser.add_argument("path", metavar="path",
+    argparser.add_argument('path', metavar='path',
                            type=str,
-                           help="the path of the domain.")
+                           help='the path of the domain.')
 
-    argparser.add_argument("module", metavar="module",
+    argparser.add_argument('module', metavar='module',
                            type=str,
-                           help="the module of the domain.",
+                           help='the module of the domain.',
                            default=None)
 
     args = argparser.parse_args()
