35de1e511a414d9bdaf265ead826def41dc34dcf
Luc Sorel-Giffo <luc.sorel@gmail.com>
2021-02-23 16:17:01 +0100

    workaround to get the cli -h and -v working when installed system-wise

--- py2puml/cli.py
+++ py2puml/cli.py
@@ -4,19 +4,14 @@
 from argparse import ArgumentParser
 
 from py2puml.py2puml import py2puml
-from py2puml import __version__, __description__
-
-CLI_VERSION = f'py2puml {__version__}'
-PATH_ARG_HELP = 'the path of the domain'
-MODULE_ARG_HELP = 'the module of the domain'
 
 
 def run():
-    argparser = ArgumentParser(description=__description__)
+    argparser = ArgumentParser(description='Generate Plantuml diagrams to document your python code')
 
-    argparser.add_argument('-v', '--version', action='version', version=CLI_VERSION)
-    argparser.add_argument('path', metavar='path', type=str, help=PATH_ARG_HELP)
-    argparser.add_argument('module', metavar='module', type=str, help=MODULE_ARG_HELP, default=None)
+    argparser.add_argument('-v', '--version', action='version', version='py2puml 0.4.0')
+    argparser.add_argument('path', metavar='path', type=str, help='the path of the domain')
+    argparser.add_argument('module', metavar='module', type=str, help='the module of the domain', default=None)
 
     args = argparser.parse_args()
     print(''.join(py2puml(args.path, args.module)))
--- tests/py2puml/test_cli.py
+++ tests/py2puml/test_cli.py
@@ -1,8 +1,9 @@
+from typing import List
 
 from pytest import mark
 from subprocess import run, PIPE
 
-from py2puml import py2puml
+from py2puml import py2puml, __version__, __description__
 
 @mark.parametrize(
     'entrypoint', [
@@ -10,7 +11,7 @@ from py2puml import py2puml
         ['python', '-m', 'py2puml']
     ]
 )
-def test_cli_consistency_with_the_default_configuration(entrypoint):
+def test_cli_consistency_with_the_default_configuration(entrypoint: List[str]):
     command = entrypoint + ['py2puml/domain', 'py2puml.domain']
     cli_stdout = run(command,
         stdout=PIPE, stderr=PIPE,
@@ -20,3 +21,41 @@ def test_cli_consistency_with_the_default_configuration(entrypoint):
     puml_content = py2puml.py2puml('py2puml/domain', 'py2puml.domain')
 
     assert ''.join(puml_content).strip() == cli_stdout.strip()
+
+@mark.parametrize(
+    'version_command', [
+        ['-v'],
+        ['--version']
+    ]
+)
+def test_cli_version(version_command: List[str]):
+    '''
+    Ensures the consistency between the CLI version and the project version set in pyproject.toml
+    which is not included when the CLI is installed system-wise
+    '''
+    command = ['py2puml'] + version_command
+    cli_version = run(command,
+        stdout=PIPE, stderr=PIPE,
+        text=True, check=True
+    ).stdout
+
+    assert cli_version == f'py2puml {__version__}\n'
+
+@mark.parametrize(
+    'help_command', [
+        ['-h'],
+        # ['--help']
+    ]
+)
+def test_cli_help(help_command: List[str]):
+    '''
+    Ensures the consistency between the CLI help and the project description set in pyproject.toml
+    which is not included when the CLI is installed system-wise
+    '''
+    command = ['py2puml'] + help_command
+    help_text = run(command,
+        stdout=PIPE, stderr=PIPE,
+        text=True, check=True
+    ).stdout
+
+    assert __description__ in help_text
