d1a58274a2a9fd18cb8fe6fe2bb7226b69c15dc7
Luc Sorel-Giffo <luc.sorel@gmail.com>
2023-08-27 16:05:16 +0200

    refactor(tests): split union-types tests in different unit tests for readability sake

--- tests/py2puml/inspection/test_inspect_union_types.py
+++ tests/py2puml/inspection/test_inspect_union_types.py
@@ -1,57 +1,81 @@
 from importlib import import_module
-from typing import Dict, List
+from typing import Callable, Dict
 
-from py2puml.domain.umlclass import UmlClass
+from pytest import fixture
+
 from py2puml.domain.umlitem import UmlItem
-from py2puml.domain.umlrelation import UmlRelation
 from py2puml.inspection.inspectmodule import inspect_module
 
 from tests.asserts.attribute import assert_attribute
 
 
-def test_inspect_dataclasses_and_class_with_union_types(
-    domain_items_by_fqn: Dict[str, UmlItem], domain_relations: List[UmlRelation]
-):
+@fixture(scope='module', autouse=True)
+def inspected_union_types_items() -> Dict[str, UmlItem]:
+    '''
+    Inspects the test module containing several types involving union types, once for all the unit tests involved in this module
+    '''
     fqdn = 'tests.modules.withuniontypes'
-    inspect_module(import_module(fqdn), fqdn, domain_items_by_fqn, domain_relations)
+    domain_items_by_fqn: Dict[str, UmlItem] = {}
+    inspect_module(import_module(fqdn), fqdn, domain_items_by_fqn, [])
 
-    assert len(domain_items_by_fqn) == 5, 'five classes must be inspected'
+    assert len(domain_items_by_fqn) == 5, 'five union-typed classes must be inspected'
 
-    # NumberWrapper UmlClass
-    umlitem: UmlClass = domain_items_by_fqn[f'{fqdn}.NumberWrapper']
+    return domain_items_by_fqn
+
+
+@fixture(scope='function')
+def get_union_type_item(inspected_union_types_items: Dict[str, UmlItem]) -> Callable[[str], UmlItem]:
+    return lambda class_name: inspected_union_types_items[f'tests.modules.withuniontypes.{class_name}']
+
+
+def test_union_type_number_wrapper(get_union_type_item: Callable[[str], UmlItem]):
+    umlitem = get_union_type_item('NumberWrapper')
+
     assert len(umlitem.attributes) == 1, '1 attribute of NumberWrapper must be inspected'
     number_attribute = umlitem.attributes[0]
     assert_attribute(number_attribute, 'number', 'Union[int, float]', expected_staticity=False)
 
-    # OptionalNumberWrapper UmlClass
-    umlitem: UmlClass = domain_items_by_fqn[f'{fqdn}.OptionalNumberWrapper']
+
+def test_union_type_optional_number_wrapper(get_union_type_item: Callable[[str], UmlItem]):
+    umlitem = get_union_type_item('OptionalNumberWrapper')
+
     assert len(umlitem.attributes) == 1, '1 attribute of OptionalNumberWrapper must be inspected'
     number_attribute = umlitem.attributes[0]
     assert_attribute(number_attribute, 'number', 'Union[int, float, None]', expected_staticity=False)
 
-    # NumberWrapperPy3_10 UmlClass
-    umlitem: UmlClass = domain_items_by_fqn[f'{fqdn}.NumberWrapperPy3_10']
+
+def test_union_type_number_wrapper_py3_10(get_union_type_item: Callable[[str], UmlItem]):
+    umlitem = get_union_type_item('NumberWrapperPy3_10')
+
     assert len(umlitem.attributes) == 1, '1 attribute of NumberWrapperPy3_10 must be inspected'
     number_attribute = umlitem.attributes[0]
     assert_attribute(number_attribute, 'number', 'int | float', expected_staticity=False)
 
-    # OptionalNumberWrapperPy3_10 UmlClass
-    umlitem: UmlClass = domain_items_by_fqn[f'{fqdn}.OptionalNumberWrapperPy3_10']
+
+def test_union_type_optional_number_wrapper_py3_10(get_union_type_item: Callable[[str], UmlItem]):
+    umlitem = get_union_type_item('OptionalNumberWrapperPy3_10')
+
     assert len(umlitem.attributes) == 1, '1 attribute of OptionalNumberWrapperPy3_10 must be inspected'
     number_attribute = umlitem.attributes[0]
     assert_attribute(number_attribute, 'number', 'int | float | None', expected_staticity=False)
 
-    # DistanceCalculator UmlClass
-    umlitem: UmlClass = domain_items_by_fqn[f'{fqdn}.DistanceCalculator']
+
+def test_union_type_distance_calculator(get_union_type_item: Callable[[str], UmlItem]):
+    umlitem = get_union_type_item('DistanceCalculator')
+
     assert len(umlitem.attributes) == 5, '5 attributes of DistanceCalculator must be inspected'
-    print(f'{umlitem.attributes=}')
+
     x_a_attribute = umlitem.attributes[0]
     assert_attribute(x_a_attribute, 'x_a', 'Union[int, float]', expected_staticity=False)
+
     y_a_attribute = umlitem.attributes[1]
     assert_attribute(y_a_attribute, 'y_a', 'Union[int, float, None]', expected_staticity=False)
+
     x_b_attribute = umlitem.attributes[2]
     assert_attribute(x_b_attribute, 'x_b', 'int | float', expected_staticity=False)
+
     y_b_attribute = umlitem.attributes[3]
     assert_attribute(y_b_attribute, 'y_b', 'int | float | None', expected_staticity=False)
+
     space_characs_attribute = umlitem.attributes[4]
     assert_attribute(space_characs_attribute, 'space_characs', 'str | None', expected_staticity=False)
