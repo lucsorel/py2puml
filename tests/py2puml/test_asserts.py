5b8a130b91c1d7e8605a44be6a4c832eb882df38
Luc Sorel-Giffo <luc.sorel@gmail.com>
2023-10-15 14:06:25 +0200

    refactor(puml): remove nesting and grouping of namespaces, use "!pragma useIntermediatePackages false" instead

---
+++ tests/py2puml/test_asserts.py
@@ -0,0 +1,22 @@
+from io import StringIO
+from typing import Iterable, List
+
+from pytest import mark
+
+from py2puml.asserts import normalize_lines_with_returns
+
+PY2PUML_HEADER = '''@startuml py2puml.domain
+!pragma useIntermediatePackages false
+'''
+
+
+@mark.parametrize(
+    ['input_lines_with_returns', 'expected_lines'], [
+        (['line'], ['line']),
+        (['line\n'], ['line', '']),
+        ([PY2PUML_HEADER], ['@startuml py2puml.domain', '!pragma useIntermediatePackages false', '']),
+        (StringIO(PY2PUML_HEADER), ['@startuml py2puml.domain', '!pragma useIntermediatePackages false', '']),
+    ]
+)
+def test_normalize_lines_with_returns(input_lines_with_returns: Iterable[str], expected_lines: List[str]):
+    assert normalize_lines_with_returns(input_lines_with_returns) == expected_lines
