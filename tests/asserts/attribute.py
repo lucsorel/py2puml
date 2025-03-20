1a3d8c088f579ff84e31c5c6275f1163c96a6e63
Mieszko <22565920+0xmzk@users.noreply.github.com>
2025-03-20 12:42:25 +0000

    Improved verbosity for failed assert_attribute fn

--- tests/asserts/attribute.py
+++ tests/asserts/attribute.py
@@ -2,6 +2,6 @@ from py2puml.domain.umlclass import UmlAttribute
 
 
 def assert_attribute(attribute: UmlAttribute, expected_name: str, expected_type: str, expected_staticity: bool):
-    assert attribute.name == expected_name
-    assert attribute.type == expected_type
-    assert attribute.static == expected_staticity
+    assert attribute.name == expected_name, f"Got '{attribute.name}' but expected '{expected_name}'"
+    assert attribute.type == expected_type, f"Got '{attribute.type}' but expected '{expected_type}'"
+    assert attribute.static == expected_staticity, f"Got '{attribute.static}' but expected '{expected_staticity}'"
