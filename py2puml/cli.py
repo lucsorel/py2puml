c377a6924bfe463927724a884830d034d09cc1fa
Luc Sorel-Giffo <luc.sorel@gmail.com>
2025-03-24 14:22:07 +0100

    docs: add Mieszko to contributors, bump version to 0.10.0

--- py2puml/cli.py
+++ py2puml/cli.py
@@ -16,7 +16,7 @@ def run():
 
     argparser = ArgumentParser(description='Generate PlantUML class diagrams to document your Python application.')
 
-    argparser.add_argument('-v', '--version', action='version', version='py2puml 0.9.1')
+    argparser.add_argument('-v', '--version', action='version', version='py2puml 0.10.0')
     argparser.add_argument('path', metavar='path', type=str, help='the filepath to the domain')
     argparser.add_argument('module', metavar='module', type=str, help='the module name of the domain', default=None)
 
